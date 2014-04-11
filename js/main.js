67337974ba335dd3f909e246df63f3aacd9fc271
mario <laserna.mario@gmail.com>
2014-04-11 18:32:52 -0500

    cambios en view

--- js/main.js
+++ js/main.js
@@ -3,15 +3,17 @@ Sfotipy = {};
 Sfotipy.Song = Backbone.Model.extend({});
 
 Sfotipy.SongView = Backbone.View.extend({
-	tagName: "li",
-	className: "item border-bottom",
-
+	tagName: 'li',
+	className: 'item border-bottom',
+	template: Handlebars.compile($("#song-template").html()),
+	
 	render: function(){
 		var song = this.model;
 		var name = song.get("name");
 		var author = song.get("author");
 
-		this.$el.html("<span>" + author + "</span> - <span>" + name + "</span");
+		var html = this.template(this.model.toJSON());
+		this.$el.html(html);
 	}
 });
 
